bfda5b2486e2f22c66e41cbe91469e71ec205094
bulkadany <73467445+bulkadany@users.noreply.github.com>
2020-11-16 17:38:22 +0500

    Update 1_2.cpp

--- Практика/1_2.cpp
+++ Практика/1_2.cpp
@@ -1,24 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+
 int a, b;
 int main()
 {
     cin >> a;
     cin >> b;
-    if (a == 0)
+    if (a == 0 && b!=0)
     {
         cout << 0;
     }
     else
-    {
+    	{
         if (b == 0)
         {
             cout << "Невозможная операция";
         }
-    }
-    if (a != 0 && b != 0)
-    {
-        cout << a / b;
-    }
+    else
+    		{
+        cout << a/b;
+    		}
     return 0;
+	}
 }
